20553e667b4053e4883ae5972c4716769e4dbc36
codecooker0105 <codecooker0105@gmail.com>
2015-05-13 02:01:54 +0800

    :sparkles: Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as renderer from 'react-test-renderer'
+
+it('renders without crashing', () => {
+
+});
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class App extends React.Component<any, any> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>DIMA</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
